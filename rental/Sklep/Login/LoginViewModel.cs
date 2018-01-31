973a9897fc4092cb41441f904f851d755070722f
Pawel Pluta <pawel.pluta@gmail.com>
2018-01-31 03:28:32 +0100

    pseudologowanie
    Dodalem pseudologowanie - najpierw pojawia sie okno logowania. Po klinieciu loguj zamyka sie i pojawia wlasciwy program. Musialem zmienic nazwe MainWindow na Window2.

---
+++ rental/Sklep/Login/LoginViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace Sklep.Login
+{
+    public class LoginViewModel
+    {
+        public void TryLogin()
+        {
+            
+            Window2 sec = new Window2();
+            Application.Current.Windows[0].Close();
+            sec.ShowDialog();
+        }
+
+        public ICommand SendTryLogin
+        {
+            get
+            {
+                return new Updater(TryLogin);
+            }
+        }
+
+    }
+}
---
+++ rental/Sklep/Login/Updater.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Sklep.Login
+{
+    public class Updater : ICommand
+    {
+        private Action commandTask;
+        private Action<Login> taskParameter;
+
+        public Updater(Action workToDo)
+        {
+            commandTask = workToDo;
+        }
+
+        public Updater(Action<Login> workToDo)
+        {
+            taskParameter = workToDo;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (commandTask != null)
+                return true;
+
+            if (parameter != null)
+                return true;
+
+            return false;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter == null)
+                commandTask();
+            else
+                taskParameter(parameter as Login);
+        }
+    }
+}
